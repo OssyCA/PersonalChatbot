3324bd2d6dc74f226be7af1d8af31dc67fe27fda
Ossy <drake@OSSMAN>
2025-04-14 20:13:18 +0200

    Changed vite config

--- LoginJwt/vite.config.js
+++ LoginJwt/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost:7289",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    cors: {
-      origin: "https://localhost:7289",
-      credentials: true,
-    },
-  },
 });
