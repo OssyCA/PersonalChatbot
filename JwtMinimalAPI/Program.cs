2e1e9da867fc250b1f13fe5d1948bdc22e970afe
Ossy <drake@OSSMAN>
2025-04-14 17:38:00 +0200

    Fixed origins

--- JwtMinimalAPI/Program.cs
+++ JwtMinimalAPI/Program.cs
@@ -36,7 +36,7 @@ namespace JwtMinimalAPI
             {
                 options.AddPolicy("AllowAllOrigins", policy =>
                 {
-                    policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "http://localhost:5174") // Update with your React app URL
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:5174") // Update with your React app URL
                           .AllowAnyMethod()
                           .AllowAnyHeader()
                           .AllowCredentials(); // This is important for cookies
