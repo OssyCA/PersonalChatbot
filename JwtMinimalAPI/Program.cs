7a8af123b0cd03f7c279a900ba9107300bdd3539
Ossy <drake@OSSMAN>
2025-04-19 01:02:09 +0200

    Updated Program.cs

--- JwtMinimalAPI/Program.cs
+++ JwtMinimalAPI/Program.cs
@@ -2,6 +2,7 @@
 using JwtMinimalAPI.Data;
 using JwtMinimalAPI.DTO;
 using JwtMinimalAPI.Endpoints;
+using JwtMinimalAPI.Extentions;
 using JwtMinimalAPI.Helpers.EmailConfig;
 using JwtMinimalAPI.Middlewere;
 using JwtMinimalAPI.Models;
@@ -16,6 +17,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using Serilog;
 using System.Text;
 using System.Threading.RateLimiting;
 
@@ -27,9 +29,24 @@ namespace JwtMinimalAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Advanced Logging
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day));
+
+          
+            builder.Services
+                .AddDatabaseServices(builder.Configuration)    // Add DbContext and Identity
+                .AddCorsPolicy()                                // Add CORS policy
+                .AuthenticationService(builder.Configuration)   // Add JWT Authentication
+                .GetMailConfig(builder.Configuration)           // Add Email Configuration
+                .AddRateLimit()                                 // Add Rate Limiting
+                .ApplicationServices();                         // Add Application Services
+
+            var app = builder.Build(); // Build the application
 
 
-            var app = builder.Build();
+            app.ConfigMiddleware().MapEndpoints(); // returns WebApplication
 
 
             app.Run();
